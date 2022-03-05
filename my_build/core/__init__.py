09c2cf708667bd582038b0f401c33b34ee62919d
guofei9987 <guofei9987@foxmail.com>
2022-03-05 23:32:59 +0800

    update

--- my_build/core/__init__.py
+++ my_build/core/__init__.py
@@ -5,14 +5,9 @@ core_path = os.path.dirname(__file__)
 
 core_files = os.listdir(core_path)
 
-so_files = dict()
-for file in core_files:
-    if file.endswith('.so'):
-        prefix = file.split('.')[0]
-        so_files[prefix] = file
+so_files = {file.split('.')[0]: os.path.join(core_path, file)
+            for file in core_files
+            if file.endswith('.so')}
 
-# read C so
-CoreFunction = ctypes.cdll.LoadLibrary(os.path.join(
-    core_path
-    , so_files['CoreFunction']
-))
+# read .so
+CoreFunction = ctypes.cdll.LoadLibrary(so_files['CoreFunction'])
